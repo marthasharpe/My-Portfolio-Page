045db8cc6f5ee9b4ccc30caf877d7e38d11e087a
Martha Sharpe <marthasharpe2020@gmail.com>
2019-11-12 11:16:41 -0500

    Import reactstrap and fontawesome

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope, faKey } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(faEnvelope, faKey);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Button className="btn-info">
+          <FontAwesomeIcon icon="envelope"/>
+            Hello
+        </Button>
+
       </header>
     </div>
   );
